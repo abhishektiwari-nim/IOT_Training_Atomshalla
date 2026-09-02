65d3c89503a8e9d28f17ee8d12a3a6731c7ff73c
Abhishek Kumar Tiwari <abhishektiwari.nim@gmail.com>
2026-09-02 09:35:04 +0530

    Add files via upload

---
+++ DynamicArray.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class DynamicArray {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+
+        System.out.println(numbers);
+    }
+}
