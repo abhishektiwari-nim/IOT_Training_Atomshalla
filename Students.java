feec906a78a558e8d1531dc1bf54633c1834fa38
Abhishek Kumar Tiwari <abhishektiwari.nim@gmail.com>
2026-09-02 09:33:22 +0530

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Students {
+    public static void main(String[] args) {
+
+        ArrayList<String> students = new ArrayList<>();
+
+        students.add("Rahul");
+        students.add("Aman");
+        students.add("Riya");
+        students.add("Priya");
+
+        System.out.println(students);
+    }
+}
